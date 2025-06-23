21d0c37a3d285cd849631567c1fe53b1f1114bc5
Tanner tahiry <tahirydev29@gmail.com>
2025-06-23 15:57:57 +0300

    feat: update Angular schematics configuration .

--- projects/tools/src/app/app.ts
+++ projects/tools/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [],
+  template: `
+  <p>{{title}}</p>
+    `,
   styleUrl: './app.scss'
 })
 export class App {
